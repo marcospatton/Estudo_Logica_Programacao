810da0dd6bd061aed4cf2719ea5f29c94263fe47
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-10-11 23:45:27 -0300

    Adicionado exercicio de repetição
    Repetição

---
+++ scriptRepeticao.js
@@ -0,0 +1,18 @@
+function pulaLinha() {
+
+    document.write("<br>");
+    document.write("<br>");
+}
+
+function mostra(frase) {
+
+    document.write(frase);
+    pulaLinha();
+}
+
+var contador = 2;
+while(contador <= 100){
+    mostra(contador);
+    contador = contador + 2; // incrementa de dois em dois
+}
+mostra("FIM");
