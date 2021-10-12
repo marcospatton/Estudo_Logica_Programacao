6180ed92da9c203292f5cbfb3ca4b6da5948ca81
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-10-12 00:43:45 -0300

    adicionado White 3 tentativas
    Adicionado o recurso de 3 tentativas de login

--- scriptTelaLogin.js
+++ scriptTelaLogin.js
@@ -1,11 +1,29 @@
-var loginCadastro ="marcospatton";
-var senhaCadastrada ="123456";
+var loginCadastrado = "marcospatton";
+    var senhaCadastrada = "123456";
 
-var loginInformado = prompt("Informe seu login");
-var senhaInformada = prompt("Informe sua senha");
+    var maximoTentativas = 3;
+    var tentativaAtual = 1;
 
-if (loginCadastro == loginInformado && senhaCadastrada == senhaInformada){
-    alert("Bem-vindo ao sistema " + loginInformado);   
-}else{
-    alert("Login inválido. tente novamente");
-}
+    while(tentativaAtual <= maximoTentativas) {
+
+        var loginInformado = prompt("Informe seu login");
+        var senhaInformada = prompt("Informe sua senha");
+
+        if( loginCadastrado == loginInformado && senhaCadastrada == senhaInformada ) {
+
+            alert("Bem-vindo ao sistema, " + loginInformado);
+
+            tentativaAtual = maximoTentativas; // acertou, então faço o gasto de todas as tentativas para sair do loop. Lá embaixo vai incrementar + 1! 
+
+        } else {
+
+            if (tentativaAtual == 3) {
+                alert("Número permitido de tentativas ultrapassado!");
+            } else {
+                alert("Login inválido. Tente novamente");
+            }
+        }
+
+       // vai para a próxima tentativa
+        tentativaAtual = tentativaAtual +1  
+    }
